04d03d4884a69055f784e8d7c28e454750e7db58
Rafael Mascarenas <rafaelmascarenhasborges@gmail.com>
2024-07-16 12:13:42 -0300

    finalization of the burguer menu and finalization of scripts

--- script.js
+++ script.js
@@ -1,80 +1,9 @@
-/*let menu = document.querySelector('#btn-menu');
-let navbar = document.querySelector('.header .navbar-mobile-cont');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.addEventListener('block')
-}
-*/
-    let menu = document.querySelector('#btn-menu');
-    let navbar = document.querySelector('.header .navbar-mobile');
-
-    menu.addEventListener('click', () => {
-        menu.classList.toggle('fa-times'); 
-        navbar.classList.toggle('navbar-active');
-    });
-
-
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-};
-
-
-var swiper = new Swiper(".home-slider", {
-    spaceBetween: 20,
-    effect: "fade",
-    grabCursor: true,
-    loop:true,
-    autoplay: {
-        delay: 4000,  
-    }, 
-    centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-});
-
-var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop:true,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    breakpoints:{
-        0:{
-            slidesPerView:1,
-        },
-        600:{
-            slidesPerView:2,
-        },
-    },
-});
-
-var swiper = new Swiper(".blogs-slider", {
-    spaceBetween: 20,
-    grabCursor: true,
-    loop:true,
-    autoplay: {
-        delay: 7500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints:{
-        0:{
-            slidesPerView:1,
-        },
-        768:{
-            slidesPerView:2,
-        },
-        991:{
-            slidesPerView:3,
-        },
-    },
-});
+document.getElementById("dropdownMenuButton").onclick = function() {
+    var dropdownMenu = document.querySelector(".dropdown-menu");
+    if (dropdownMenu.style.display === "none" || dropdownMenu.style.display === "") {
+      dropdownMenu.style.display = "block";
+    } else {
+      dropdownMenu.style.display = "none";
+    }
+  }
